c381c261552ec04679a43f4b068db66b143b4c1c
celerno@gmail.com <celerno@gmail.com>
2023-03-28 20:51:09 -0700

    style to main menu. general style. rename template

--- backend/settings.py
+++ backend/settings.py
@@ -158,11 +158,12 @@ PARLER_LANGUAGES = {
     }
 }
 CMS_TEMPLATES = [
+     # optional templates that extend base.html, to be used with Bootstrap 5
+    ('aquiestan.html', 'Aquí están base'),
     # a minimal template to get started with
     ('minimal.html', 'Minimal template'),
 
-    # optional templates that extend base.html, to be used with Bootstrap 5
-    ('bootstrap5.html', 'Bootstrap 5 Demo'),
+   
 
     # serving static files with whitenoise demo
     ('whitenoise-static-files-demo.html', 'Static File Demo'),
