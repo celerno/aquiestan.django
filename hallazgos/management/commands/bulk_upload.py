be1d5d8798353435d7136f869684e5d705679a2c
celernowgu <celerino.herrera@wgu.edu>
2023-04-19 09:46:45 -0700

    fix fecha con update_or_create

--- hallazgos/management/commands/bulk_upload.py
+++ hallazgos/management/commands/bulk_upload.py
@@ -25,42 +25,29 @@ class Command(BaseCommand):
             next(csv_reader) # Skip header
             for row in csv_reader:
                 if row:
-                    #SOURCE_ID	COLECTIVO	FECHA	Municipio	Localidad	latitud longitud		TIPO HALLAZGO	MODALIDAD DE HALLAZGO	OBSERVACIONES	INFORMACIÓN ADICIONAL 	FUENTE	LINK 	FOTOS
-                    hallazgo = Hallazgo(source_id = row[0].strip(),
-                                        colectivo = Colectivo.exist_or_create(nombre= row[1].strip()), 
-                                        fecha = parse_date(row[2]),
-                                        municipio =  Municipio.exist_or_create(row[3].strip()), 
-                                        localidad = row[4].strip(),
-                                        tipo = row[7].strip().capitalize(),
-                                        modalidad = Modalidad.exist_or_create(row[8].strip().capitalize()), 
-                                        observaciones = row[9].strip(), 
-                                        informacion_adicional = row[10].strip(), 
-                                        fuente = row[11].strip(), 
-                                        link = row[12].strip(),
-                                        contiene_imagenes = True if row[13] else False,
-                                        notas_internas = row[14].strip(),
-                                        )
-                    try:
-                        hallazgo.geo_latitud = 0 if not row[5] else row[5].strip()
-                    except ValueError:
-                        try:
-                            hallazgo.geo_latitud = 0 if not row[5] else row[5].split(",")[0].strip()
-                            hallazgo.geo_longitud = 0 if not row[5] else row[5].split(",")[1].strip()
-                        except ValueError:
-                            hallazgo.geo_latitud = 0
-                    if not hallazgo.geo_longitud or hallazgo.geo_longitud == 0:
-                        try:
-                            hallazgo.geo_longitud = 0 if not row[6] else row[6].strip()
-                        except ValueError:
-                            hallazgo.geo_longitud = 0
-                                                            
-
-                        hallazgo.fecha = parse_date(row[2])
                     try:
                         with transaction.atomic():
-                            hallazgo.save()
-                    except Exception as e:
-                        logger.error('Error al cargar hallazgo: %s', e)
+                            #SOURCE_ID	COLECTIVO	FECHA	Municipio	Localidad	latitud longitud		TIPO HALLAZGO	MODALIDAD DE HALLAZGO	OBSERVACIONES	INFORMACIÓN ADICIONAL 	FUENTE	LINK 	FOTOS
+                            hallazgo, created = Hallazgo.objects.update_or_create(
+                                source_id = row[0].strip(),
+                                defaults = { "colectivo" : Colectivo.exist_or_create( nombre =row[1].strip()),
+                                            "fecha" : parse_date(row[2]),
+                                            "municipio" :  Municipio.exist_or_create(row[3].strip()), 
+                                            "localidad" : row[4].strip(),
+                                            "tipo" : row[7].strip().capitalize(),
+                                            "modalidad" : Modalidad.exist_or_create(row[8].strip().capitalize()), 
+                                            "observaciones" : row[9].strip(),
+                                            "informacion_adicional" : row[10].strip(),
+                                            "fuente" : row[11].strip(),
+                                            "link" : row[12].strip(),
+                                            "contiene_imagenes" : True if row[13] else False,
+                                            "notas_internas" : row[14].strip(),
+                                            "geo_latitud": 0 if not row[5] else row[5].strip(),
+                                            "geo_longitud":  0 if not row[6] else row[6].strip(),
+                                            }
+                                )
+                    except Exception as exception:
+                        logger.error('Error al cargar hallazgo: %s', exception)
                     else:
                         logger.info('Hallazgo cargado: %s', hallazgo)
                     
